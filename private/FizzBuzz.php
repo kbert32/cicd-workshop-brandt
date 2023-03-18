a13a9ff121ecae28bee67935855bac5c44b54a8b
kbert32 <103608624+kbert32@users.noreply.github.com>
2023-03-18 12:27:06 -0600

    cd

--- private/FizzBuzz.php
+++ private/FizzBuzz.php
@@ -7,7 +7,7 @@
             for ($i=$fromNum; $i <= $toNum; $i++) {
                 $fizzBuzzString = "$i";
 
-                if ($i % 3 == 0 || $i % 5 == 0) {
+                if ($ix % 3 == 0 || $i % 5 == 0) {
                     $fizzBuzzString .= " - ";
                 }
                 
